d7712038cafedeca56bf229ef22d3d66b685f652
Sergey Iryupin <biblelamp@yandex.ru>
2026-04-08 07:24:50 +0200

    version 0.5.1

--- html/sitemap.php
+++ html/sitemap.php
@@ -1,5 +1,5 @@
 <?php
-// sitemap.php version 0.4 by 6-Apr-26
+// sitemap.php version 0.5 by 7-Apr-26
 
 header('Content-Type: application/xml; charset=utf-8');
 
@@ -16,8 +16,10 @@ $urls = [
     '/about/',
     '/events/',
     '/reports/',
-    '/authors/'
+    '/authors/',
+    '/gallery/'
 ];
+$static_pages = count($urls);
 
 // add events pages
 $events = readBlocks('data/events.txt');
@@ -26,6 +28,7 @@ foreach ($events as $item) {
     $name      = $item[$name_event_idx] ?? '';
     $urls[]    = generateUrl($date_time[0], 'events', $name);
 }
+$event_pages = count($urls) - $static_pages;
 
 // add reports pages
 $reports = readBlocks('data/reports.txt');
@@ -34,6 +37,7 @@ foreach ($reports as $item) {
     $name   = $item[$name_report_idx] ?? '';
     $urls[] = generateUrl($date, 'reports', $name);
 }
+$report_pages = count($urls) - $static_pages - $event_pages;
 
 // add authors pages
 $authors = readBlocks('data/authors.txt');
@@ -43,12 +47,29 @@ foreach ($authors as $item) {
         $urls[] = '/authors/' . urlencode($nickname[0]);
     }
 }
+$author_pages = count($urls) - $static_pages - $event_pages - $report_pages;
+
+// add gallery pages
+$iterator = new DirectoryIterator('data/gallery/');
+foreach ($iterator as $file) {
+    if ($file->isDir() && !$file->isDot()) {
+        $urls[] = '/gallery/' . $file->getFilename();
+    }
+}
+$gallery_pages = count($urls) - $static_pages - $event_pages - $report_pages - $author_pages;
+
+// TODO add galleries items
 
 // XML header
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
+    <!-- <?= count($urls) ?> pages -->
+    <!-- <?= $static_pages ?> static pages -->
+    <!-- <?= $event_pages ?> event pages -->
+    <!-- <?= $report_pages ?> report pages -->
+    <!-- <?= $author_pages ?> author pages -->
+    <!-- <?= $gallery_pages ?> gallery pages -->
 <?php foreach ($urls as $url): ?>
     <url>
         <loc><?= htmlspecialchars($baseUrl . $url) ?></loc>
